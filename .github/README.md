36605e8c5441a414478e3b6bbd54365134d3bbf2
unknown <luizeduardoduarte96@outlook.com>
2023-10-30 16:05:17 -0300

    first commit

---
+++ .github/README.md
@@ -0,0 +1,22 @@
+<h1 align="center"> DevLinks </h1>
+
+<p align="center">
+Projeto com o objetivo de destacar opcoes relevantes, reduzindo a dificuldade de encontrar um determinado assunto.
+</p>
+<br>
+
+## Tecnologia
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+- HTML e CSS
+- JavaScript
+- Git e Github
+- Figma
+
+## Projeto
+O DevLinks é um agregador de links para usar como cartão de visitas online, que sao conhecido como LinkTree.
+- [Acesse o projeto finalizado ](https://github.com/luizeduardoduarte/devlinks)
+
+## Design
+<p align="center">
+  <img alt="projeto DevLinks" src="preview.jpg" width="50%">
+</p>
--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle('light')
-  
-  const img = document.querySelector("#profile img")
+  const html = document.documentElement// selecionando arquivo
+  html.classList.toggle("light")  // botao para transacao de Mode Dark/Light
 
-  if(html.classList.contains('light')) {
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
-    img.setAttribute("alt", "Descricao Light")
+  const img = document.querySelector("#profile img") // selecionando a imagem do profile
+
+  // troca de imagens
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/img_direito.jpg")
+    img.setAttribute("alt", "Descricao Light") 
   } else {
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./assets/assets/img_esquerdo.jpg")
     img.setAttribute("alt", "Descricao Dark")
   }
-
 }
